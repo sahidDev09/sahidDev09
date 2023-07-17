0434fdf687f3e7466e162bd7a9667ab6e02202ba
MD SAHID AHMED <122889817+sahidDev09@users.noreply.github.com>
2023-07-17 13:44:13 +0600

    Add files via upload

---
+++ FoodData.js
@@ -0,0 +1,68 @@
+const FoodDataJson = [
+    {
+      id: 1,
+      name: "Love Bite Pizza",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      price: "500",
+      img: "../images/pizza.png",
+    },
+  
+    {
+      id: 2,
+      name: "Cheese Burger",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id",
+      price: "360",
+      img: "../images/burger.png",
+    },
+  
+    {
+      id: 3,
+      name: "Burger set-1",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id",
+      price: "299",
+      img: "../images/mealbox.png",
+    },
+  
+    {
+      id: 4,
+      name: "Biriyani",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing eli",
+      price: "540",
+      img: "../images/biriyani.png",
+    },
+  
+    {
+      id: 5,
+      name: "Chawmein",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit!",
+      price: "600",
+      img: "../images/chawmeen.png",
+    },
+  
+    {
+      id: 6,
+      name: "Indian Lunch",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing ",
+      price: "400",
+      img: "../images/indianlunch.png",
+    },
+  
+    {
+      id: 7,
+      name: "Platter",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
+      price: "900",
+      img: "../images/splater.png",
+    },
+  
+    {
+      id: 8,
+      name: "Pinapple Juice",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
+      price: "85",
+      img: "../images/pinapplejuice.png",
+    },
+  ];
+  
+  export default FoodDataJson;
+
